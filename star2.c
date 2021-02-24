03546e5bf048fb45bc0bda9658fba26ac2b53b9f
shantanu2019 <49944753+shantanu2019@users.noreply.github.com>
2021-02-25 00:36:59 +0530

    Add files via upload

---
+++ star2.c
@@ -0,0 +1,13 @@
+main()
+{
+    int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+      {
+          printf("");
+          for(j=0;j<=i;j++)
+            printf("%d",*);
+          printf("\n");
+      }
+    }
